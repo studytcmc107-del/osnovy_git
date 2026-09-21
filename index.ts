94a52ed93a76060029aac9ae336e0d4577ee06f0
Nastya <studytcmc107@gmail.com>
2026-09-21 22:18:13 +0300

    Очищено под меню

--- index.ts
+++ index.ts
@@ -9,12 +9,3 @@ function showMenu(): void {
 
 showMenu();
 
-function add(a: number, b: number): number {
-    return a + b;
-}
-
-function subtract(a: number, b: number): number {
-    return a - b;
-}
-
-showMenu();
