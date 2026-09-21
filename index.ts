5ccc773203c02b87ef08511828817c3db37e98dc
Nastya <studytcmc107@gmail.com>
2026-09-21 21:52:46 +0300

    Добавлены функции

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Меню калькулятора");
+function showMenu(): void {
+    console.log("Меню");
+    console.log("1. Сложение");
+    console.log("2. Вычитание");
+    console.log("3. Умножение");
+    console.log("4. Деление");
+    console.log("0. Выход");
+}
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+showMenu();
